34f2d41c9f5686e06cbaa3bf230738e3d187922e
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-08-26 12:47:49 +0530

    ''

---
+++ main19/src/app/layouts/full/sidebar/branding.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
+
+@Component({
+  selector: 'app-branding',
+  imports: [],
+  template: `
+    <a href="/" class="logodark">
+      <img
+        src="./assets/images/logos/dark-logo.svg"
+        class="align-middle m-2"
+        alt="logo"
+      />
+    </a>
+  `,
+})
+export class BrandingComponent {
+  options = this.settings.getOptions();
+  constructor(private settings: CoreService) {}
+}
---
+++ main19/src/app/layouts/full/top-strip/topstrip.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { TablerIconsModule } from 'angular-tabler-icons';
+
+@Component({
+    selector: 'app-topstrip',
+    imports: [TablerIconsModule, MatButtonModule, MatMenuModule],
+    templateUrl: './topstrip.component.html',
+})
+export class AppTopstripComponent {
+    constructor() { }
+
+}
+ 
